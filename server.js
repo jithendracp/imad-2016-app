495f18223f5c19e42bc4d442dbe50a06d32c506a
jithendracp <jithudamodar@gmail.com>
2016-10-11 21:57:41 +0600

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-oe',function(req,res)
+app.get('/article-one',function(req,res)
 {
     res.send('REQUEST FOR FIRST FILE');
 });
